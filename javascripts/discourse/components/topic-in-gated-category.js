387b35acc620f8af4a8db45e91450753c640b3ed
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-19 13:57:40 -0700

    logging what is in the component

--- javascripts/discourse/components/topic-in-gated-category.js
+++ javascripts/discourse/components/topic-in-gated-category.js
@@ -25,6 +25,7 @@ export default Component.extend({
   },
 
   recalculate() {
+    console.log(this);
     // do nothing if:
     // a) topic does not have a category
     // b) component setting is empty
