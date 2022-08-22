0ebd9498743397d5f7aff63e942ae07f8d674bd5
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-22 15:29:50 -0700

    adjust the position of the scroll

--- javascripts/discourse/api-initializers/init-topic-in-gated-category.js
+++ javascripts/discourse/api-initializers/init-topic-in-gated-category.js
@@ -5,7 +5,7 @@ import {
 export default apiInitializer("0.8", (api) => {
   if (window.location.pathname.includes('/t/')) {
     document.addEventListener("DOMContentLoaded", () => {
-      let element = document.querySelector('.cta-section-connector');
+      let element = document.querySelector('.below-site-header-outlet');
       element.scrollIntoView();
       // window.scrollBy(0, -100);
       // let styleSheet = document.createElement("style");
