44d2b8e5a986dea156088efe21bc5895eedb34a3
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-22 13:37:14 -0700

    add a class to the main content wrapper on if a category is gated

--- javascripts/discourse/api-initializers/init-topic-in-gated-category.js
+++ javascripts/discourse/api-initializers/init-topic-in-gated-category.js
@@ -6,6 +6,8 @@ export default apiInitializer("0.8", (api) => {
   api.onPageChange(() => {
     if (window.location.pathname.includes('/t/')) {
       document.querySelector("#main-outlet").classList.add("with-gated-category");
+    } else {
+      document.querySelector("#main-outlet").classList.remove("with-gated-category");
     }
   })
 })
