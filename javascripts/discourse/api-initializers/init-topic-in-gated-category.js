3754b1fee75f320303a09538c4bdbd83b0235cef
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-22 15:24:28 -0700

    restyle gated category theme

--- javascripts/discourse/api-initializers/init-topic-in-gated-category.js
+++ javascripts/discourse/api-initializers/init-topic-in-gated-category.js
@@ -5,8 +5,8 @@ import {
 export default apiInitializer("0.8", (api) => {
   if (window.location.pathname.includes('/t/')) {
     document.addEventListener("DOMContentLoaded", () => {
-      let element = document.querySelector('.join-conversation-container');
-      element.scrollIntoView();
+      let element = document.querySelector('.cta-section-connector');
+      element.scrollIntoView({block:"end"});
       // window.scrollBy(0, -100);
       // let styleSheet = document.createElement("style");
       // styleSheet.innerHTML = ".topic-in-gated-category { }"
